2951fa10034342395fd8b38349b319fd22138f0d
behrooz bozorg chami <behroozbozorgchami@hotmail.com>
2021-07-24 11:52:47 +0430

    Sms helper

---
+++ src/SMSHelper/SMSHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace SMSHelper
+{
+    public class SMSHelper
+    {
+        //================> Used to encoding GSM message as UCS2
+        public static string UnicodeStr2HexStr(string strMessage)
+        {
+            byte[] ba = Encoding.BigEndianUnicode.GetBytes(strMessage);
+            string strHex = BitConverter.ToString(ba);
+            strHex = strHex.Replace("-", "");
+            return strHex;
+        }
+
+        public static string HexStr2UnicodeStr(string strHex)
+        {
+            byte[] ba = HexStr2HexBytes(strHex);
+            return HexBytes2UnicodeStr(ba);
+        }
+
+        //================> Used to decoding GSM UCS2 message  
+        public static string HexBytes2UnicodeStr(byte[] ba)
+        {
+            var strMessage = Encoding.BigEndianUnicode.GetString(ba, 0, ba.Length);
+            return strMessage;
+        }
+
+        public static byte[] HexStr2HexBytes(string strHex)
+        {
+            strHex = strHex.Replace(" ", "");
+            int nNumberChars = strHex.Length / 2;
+            byte[] aBytes = new byte[nNumberChars];
+            using (var sr = new StringReader(strHex))
+            {
+                for (int i = 0; i < nNumberChars; i++)
+                    aBytes[i] = Convert.ToByte(new string(new char[2] { (char)sr.Read(), (char)sr.Read() }), 16);
+            }
+            return aBytes;
+        }
+    }
+}
